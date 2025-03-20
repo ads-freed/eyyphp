137edfd6fc9a3ef908ef0c61c487b7aeec42eaae
ads-freed <57936630+ads-freed@users.noreply.github.com>
2025-03-20 15:57:26 +0800

    Create customer_dashboard.php

---
+++ app/views/dashboard/customer_dashboard.php
@@ -0,0 +1,30 @@
+<?php include '../app/views/layouts/header.php'; ?>
+<div class="container">
+    <h2>Customer Dashboard</h2>
+    <div>
+        <h3>Your Tickets</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Ticket ID</th>
+                    <th>Title</th>
+                    <th>Status</th>
+                    <th>Priority</th>
+                    <th>Created At</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($tickets as $ticket): ?>
+                <tr>
+                    <td><?php echo $ticket['id']; ?></td>
+                    <td><a href="/view-ticket?id=<?php echo $ticket['id']; ?>"><?php echo $ticket['title']; ?></a></td>
+                    <td><?php echo $ticket['status']; ?></td>
+                    <td><?php echo $ticket['priority']; ?></td>
+                    <td><?php echo $ticket['created_at']; ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+<?php include '../app/views/layouts/footer.php'; ?>
