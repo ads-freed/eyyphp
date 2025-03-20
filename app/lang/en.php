2be00d209c7cf1679338c0028e867b375ba5186a
ads-freed <57936630+ads-freed@users.noreply.github.com>
2025-03-20 15:50:25 +0800

    Delete app directory

--- app/lang/en.php
+++
@@ -1,8 +0,0 @@
-<?php
-// app/lang/en.php
-return [
-    'welcome'   => 'Welcome',
-    'login'     => 'Login',
-    'register'  => 'Register',
-    // … add additional translations as needed
-];
--- app/lang/ja.php
+++
@@ -1,8 +0,0 @@
-<?php
-// app/lang/ja.php
-return [
-    'welcome'   => 'ようこそ',
-    'login'     => 'ログイン',
-    'register'  => '登録',
-    // … add additional translations as needed
-];
